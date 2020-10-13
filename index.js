4ae7b290eff716acad82c053a2b4c15c419d0a28
william <4269946@qq.com>
2020-10-13 21:56:00 +0800

    fix import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Encryptor from './ecb-encryptor.js';
 import Decryptor from './ecb-decryptor.js';
-import { convertUtf8, convertHex } from './utils.js';
+import { convertUtf8, convertHex, pkcs7pad, pkcs7strip } from './utils.js';
 
 export { Encryptor, Decryptor, convertUtf8, convertHex, pkcs7pad, pkcs7strip };
 
